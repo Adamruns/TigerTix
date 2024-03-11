2512930bb49ad9b642ebbae3df49cb9ab4478834
Adam Smith <ars022703@yahoo.com>
2024-03-11 14:58:03 -0400

    Localized nuget config

--- TigerTix.Web/obj/Debug/net8.0/TigerTix.Web.AssemblyInfo.cs
+++ TigerTix.Web/obj/Debug/net8.0/TigerTix.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TigerTix.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a32de96c483481b69a00e86c10da37e483974b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51bae6b841b47100bbd523d3a76b00ecfb07bf7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TigerTix.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TigerTix.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
